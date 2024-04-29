cb80610045f6190d4a3a00422898898183e77fe8
SA22225337 <tyh225337@mail.ustc.edu.cn>
2024-04-29 22:42:06 +0800

    项目初始化

---
+++ src/main/java/com/tanyinghao/MyBlog202404Application.java
@@ -0,0 +1,13 @@
+package com.tanyinghao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlog202404Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlog202404Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tanyinghao/MyBlog202404ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanyinghao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlog202404ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
