34e39e4508819dca3bad6b6c76138844559992a1
SA22225337 <tyh225337@mail.ustc.edu.cn>
2024-05-03 23:23:49 +0800

    登录模块--用户模块 redis服务相关接口封装实现

--- src/main/java/com/tanyinghao/service/Impl/RedisServiceImpl.java
+++ src/main/java/com/tanyinghao/service/Impl/RedisServiceImpl.java
@@ -1,13 +1,14 @@
 package com.tanyinghao.service.Impl;
 
 import com.tanyinghao.service.RedisService;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import javax.annotation.Resource;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * @ClassName RedisServiceImpl
@@ -18,208 +19,223 @@ import java.util.concurrent.TimeUnit;
  **/
 @Service
 public class RedisServiceImpl implements RedisService {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
     @Override
     public Boolean setExpire(String key, long timeout, TimeUnit timeUnit) {
-        return null;
+        return redisTemplate.expire(key,timeout,timeUnit);
     }
 
     @Override
     public Long getExpire(String key, TimeUnit timeUnit) {
-        return null;
+        return redisTemplate.getExpire(key,timeUnit);
     }
 
     @Override
     public Collection<String> getKeys(String pattern) {
-        return null;
+        return redisTemplate.keys(pattern);
     }
 
     @Override
     public Boolean hasKey(String key) {
-        return null;
+        return redisTemplate.hasKey(key);
     }
 
     @Override
     public <T> void setObject(String key, T value) {
-
+        redisTemplate.opsForValue().set(key,value);
     }
 
     @Override
     public <T> void setObject(String key, T value, long timeout, TimeUnit timeUnit) {
-
+        redisTemplate.opsForValue().set(key,value,timeout,timeUnit);
     }
 
     @Override
     public <T> T getObject(String key) {
-        return null;
+        return (T) redisTemplate.opsForValue().get(key);
     }
 
     @Override
     public Boolean deleteObject(String key) {
-        return null;
+        return redisTemplate.delete(key);
     }
 
     @Override
     public Long deleteObject(List<String> keys) {
-        return null;
+        return redisTemplate.delete(keys);
     }
 
     @Override
     public Long incr(String key, long delta) {
-        return null;
+        return redisTemplate.opsForValue().increment(key,delta);
     }
 
     @Override
     public Long decr(String key, long delta) {
-        return null;
+        return redisTemplate.opsForValue().decrement(key,delta);
     }
 
     @Override
     public <T> void setHash(String key, String hashKey, T value) {
-
+        redisTemplate.opsForHash().put(key,hashKey,value);
     }
 
     @Override
     public <T> Boolean setHash(String key, String hashKey, T value, long timeout, TimeUnit timeUnit) {
-        return null;
+        redisTemplate.opsForHash().put(key,hashKey,value);
+        return setExpire(key,timeout,timeUnit);
     }
 
     @Override
     public <T> void setHashAll(String key, Map<String, T> map) {
-
+        redisTemplate.opsForHash().putAll(key,map);
     }
 
     @Override
     public <T> Boolean setHashAll(String key, Map<String, T> map, long timeout, TimeUnit timeUnit) {
-        return null;
+        redisTemplate.opsForHash().putAll(key,map);
+        return setExpire(key,timeout,timeUnit);
     }
 
     @Override
     public <T> T getHash(String key, String hashKey) {
-        return null;
+        return (T) redisTemplate.opsForHash().get(key, hashKey);
     }
 
     @Override
-    public <T> Map<String, T> getHashAll(String key) {
-        return null;
+    public Map getHashAll(String key) {
+        return redisTemplate.opsForHash().entries(key);
     }
 
     @Override
     public <T> void deleteHash(String key, T... hashKeys) {
-
+        redisTemplate.opsForHash().delete(key, hashKeys);
     }
 
     @Override
     public Boolean hasHashValue(String key, String hashKey) {
-        return null;
+        return redisTemplate.opsForHash().hasKey(key,hashKey);
     }
 
     @Override
     public Long incrHash(String key, String hashKey, Long delta) {
-        return null;
+        return redisTemplate.opsForHash().increment(key,hashKey,delta);
     }
 
     @Override
     public Long decrHash(String key, String hashKey, Long delta) {
-        return null;
+        return redisTemplate.opsForHash().increment(key,hashKey,-delta);
     }
 
     @Override
     public <T> Long setList(String key, T value) {
-        return null;
+        return redisTemplate.opsForList().rightPush(key,value);
     }
 
     @Override
     public <T> Long setList(String key, T value, long timeout, TimeUnit timeUnit) {
-        return null;
+        Long count = redisTemplate.opsForList().rightPush(key, value);
+        setExpire(key,timeout,timeUnit);
+        return count;
     }
 
     @Override
     public <T> Long setListAll(String key, T... values) {
-        return null;
+        return redisTemplate.opsForList().rightPushAll(key,values);
     }
 
     @Override
     public <T> Long setListAll(String key, long timeout, TimeUnit timeUnit, T... values) {
-        return null;
+        Long count = redisTemplate.opsForList().rightPushAll(key, values);
+        setExpire(key,timeout,timeUnit);
+        return count;
     }
 
     @Override
     public <T> List<T> getList(String key, long start, long end) {
-        return null;
+        return (List <T>) redisTemplate.opsForList().range(key,start,end);
     }
 
     @Override
     public <T> T getListByIndex(String key, long index) {
-        return null;
+        return (T) redisTemplate.opsForList().index(key,index);
     }
 
     @Override
     public Long getListSize(String key) {
-        return null;
+        return redisTemplate.opsForList().size(key);
     }
 
     @Override
     public <T> Long deleteList(String key, long count, T value) {
-        return null;
+        return redisTemplate.opsForList().remove(key,count,value);
     }
 
     @Override
     public <T> Long setSet(String key, T... values) {
-        return null;
+        return redisTemplate.opsForSet().add(key,values);
     }
 
     @Override
     public <T> Long setSet(String key, long timeout, TimeUnit timeUnit, T... values) {
-        return null;
+        Long count = redisTemplate.opsForSet().add(key, values);
+        setExpire(key,timeout,timeUnit);
+        return count;
     }
 
     @Override
     public <T> Set<T> getSet(String key) {
-        return null;
+        return (Set<T>) redisTemplate.opsForSet().members(key);
     }
 
     @Override
     public <T> Long deleteSet(String key, T... values) {
-        return null;
+        return redisTemplate.opsForSet().remove(key,values);
     }
 
     @Override
     public <T> Boolean hasSetValue(String key, T value) {
-        return null;
+        return redisTemplate.opsForSet().isMember(key,value);
     }
 
     @Override
     public Long getSetSize(String key) {
-        return null;
+        return redisTemplate.opsForSet().size(key);
     }
 
     @Override
     public <T> Double incrZet(String key, T value, Double score) {
-        return null;
+        return redisTemplate.opsForZSet().incrementScore(key,value, score);
     }
 
     @Override
     public <T> Double decrZet(String key, T value, Double score) {
-        return null;
+        return redisTemplate.opsForZSet().incrementScore(key,value,-score);
     }
 
     @Override
     public <T> Long deleteZetScore(String key, T... values) {
-        return null;
+        return redisTemplate.opsForZSet().remove(key,values);
     }
 
     @Override
     public Map<Object, Double> zReverseRangeWithScore(String key, long start, long end) {
-        return null;
+        return redisTemplate.opsForZSet().reverseRangeWithScores(key,start,end)
+                .stream()
+                .collect(Collectors.toMap(ZSetOperations.TypedTuple::getValue, ZSetOperations.TypedTuple::getScore));
     }
 
     @Override
     public <T> Double getZsetScore(String key, T value) {
-        return null;
+        return redisTemplate.opsForZSet().score(key,value);
     }
 
     @Override
     public Map<Object, Double> getZsetAllScore(String key) {
-        return null;
+        return Objects.requireNonNull(redisTemplate.opsForZSet().rangeWithScores(key, 0, -1))
+                .stream()
+                .collect(Collectors.toMap(ZSetOperations.TypedTuple::getValue, ZSetOperations.TypedTuple::getScore));
     }
 }
