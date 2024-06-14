92958965dbd41bdbcf8367dcd4431f18810315de
falmehknight <1206598310@qq.com>
2024-06-14 10:52:20 +0800

    说说模块--后台管理模块函数实现

--- src/main/java/com/tanyinghao/mapper/TalkMapper.java
+++ src/main/java/com/tanyinghao/mapper/TalkMapper.java
@@ -2,8 +2,35 @@ package com.tanyinghao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tanyinghao.model.entity.Talk;
+import com.tanyinghao.model.vo.TalkBackInfoVO;
+import com.tanyinghao.model.vo.TalkBackVO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TalkMapper extends BaseMapper<Talk> {
+
+    /**
+     *
+     * @Author TanYingHao
+     * @Description 分页查询说说列表
+     * @Date 10:34 2024/6/14
+     * @Param [limit, size, status]
+     * @return java.util.List<com.tanyinghao.model.vo.TalkBackVO>
+     **/
+    List<TalkBackVO> selectTalkBackVO(@Param("limit") Long limit, @Param("size") Long size, @Param("status") Integer status);
+
+    /**
+     *
+     * @Author TanYingHao
+     * @Description 根据id查询说说信息
+     * @Date 10:47 2024/6/14
+     * @Param [talkId]
+     * @return com.tanyinghao.model.vo.TalkBackInfoVO
+     **/
+    @Select("SELECT id, talk_content, images, is_top, `status` FROM t_talk WHERE id = #{talkId}")
+    TalkBackInfoVO selectTalkBackById(@Param("talkId") Integer talkId);
 }
--- src/main/java/com/tanyinghao/service/Impl/TalkServiceImpl.java
+++ src/main/java/com/tanyinghao/service/Impl/TalkServiceImpl.java
@@ -1,17 +1,34 @@
 package com.tanyinghao.service.Impl;
 
+import cn.dev33.satoken.stp.StpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tanyinghao.comm.enums.FilePathEnum;
+import com.tanyinghao.comm.utils.BeanCopyUtils;
+import com.tanyinghao.comm.utils.CommUtils;
+import com.tanyinghao.comm.utils.FileUtils;
+import com.tanyinghao.comm.utils.PageUtils;
+import com.tanyinghao.mapper.BlogFileMapper;
 import com.tanyinghao.mapper.TalkMapper;
 import com.tanyinghao.model.dto.ConditionDTO;
 import com.tanyinghao.model.dto.TalkDTO;
+import com.tanyinghao.model.entity.BlogFile;
 import com.tanyinghao.model.entity.Talk;
 import com.tanyinghao.model.vo.PageResult;
 import com.tanyinghao.model.vo.TalkBackInfoVO;
 import com.tanyinghao.model.vo.TalkBackVO;
-import com.tanyinghao.service.TagService;
 import com.tanyinghao.service.TalkService;
+import com.tanyinghao.strategy.context.UploadStrategyContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+import static com.tanyinghao.comm.constant.CommConstant.FALSE;
 
 /**
  * @ClassName TalkServiceImpl
@@ -22,33 +39,93 @@ import org.springframework.web.multipart.MultipartFile;
  **/
 @Service
 public class TalkServiceImpl extends ServiceImpl<TalkMapper, Talk> implements TalkService {
+
+    @Autowired
+    private TalkMapper talkMapper;
+
+    @Autowired
+    private BlogFileMapper blogFileMapper;
+
+    @Autowired
+    private UploadStrategyContext uploadStrategyContext;
+
     @Override
     public PageResult<TalkBackVO> listTalkBackVO(ConditionDTO condition) {
-        return null;
+        // 查询说说数量
+        Long count = talkMapper.selectCount(new LambdaQueryWrapper<Talk>()
+                .eq(Objects.nonNull(condition.getStatus()), Talk::getStatus, condition.getStatus()));
+        if (count == 0) {
+            return new PageResult<>();
+        }
+        // 分页查询说说列表
+        List<TalkBackVO> talkBackVOList = talkMapper.selectTalkBackVO(PageUtils.getLimit(),
+                PageUtils.getSize(), condition.getStatus());
+        talkBackVOList.forEach(item -> {
+            // 转换图片格式
+            if (Objects.nonNull(item.getImages())) {
+                item.setImgList(CommUtils.castList(JSON.parseObject(item.getImages(), List.class), String.class));
+            }
+        });
+        return new PageResult<>(talkBackVOList, count);
     }
 
     @Override
     public String uploadTalkCover(MultipartFile file) {
-        return null;
+        // 上传文件
+        String url = uploadStrategyContext.executeUploadStrategy(file, FilePathEnum.TALK.getPath());
+        try {
+            // 获取文件md5值
+            String md5 = FileUtils.getMd5(file.getInputStream());
+            // 获取文件扩展名
+            String extName = FileUtils.getExtension(file);
+            BlogFile existFile = blogFileMapper.selectOne(new LambdaQueryWrapper<BlogFile>()
+                    .select(BlogFile::getId)
+                    .eq(BlogFile::getFileName, md5)
+                    .eq(BlogFile::getFilePath, FilePathEnum.TALK.getFilePath()));
+            if (Objects.isNull(existFile)) {
+                // 保存文件信息
+                BlogFile newFile = BlogFile.builder()
+                        .fileUrl(url)
+                        .fileName(md5)
+                        .filePath(FilePathEnum.TALK.getFilePath())
+                        .extendName(extName)
+                        .fileSize((int) file.getSize())
+                        .isDir(FALSE)
+                        .build();
+                blogFileMapper.insert(newFile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return url;
     }
 
     @Override
     public void addTalk(TalkDTO talk) {
-
+        Talk newTalk = BeanCopyUtils.copyBean(talk, Talk.class);
+        newTalk.setUserId(StpUtil.getLoginIdAsInt());
+        baseMapper.insert(newTalk);
     }
 
     @Override
     public void deleteTalk(Integer talkId) {
-
+        talkMapper.deleteById(talkId);
     }
 
     @Override
     public void updateTalk(TalkDTO talk) {
-
+        Talk newTalk = BeanCopyUtils.copyBean(talk, Talk.class);
+        newTalk.setUserId(StpUtil.getLoginIdAsInt());
+        baseMapper.updateById(newTalk);
     }
 
     @Override
     public TalkBackInfoVO editTalk(Integer talkId) {
-        return null;
+        TalkBackInfoVO talkBackInfoVO = talkMapper.selectTalkBackById(talkId);
+        // 转换图片格式
+        if (Objects.nonNull(talkBackInfoVO.getImages())) {
+            talkBackInfoVO.setImgList(CommUtils.castList(JSON.parseObject(talkBackInfoVO.getImages(), List.class), String.class));
+        }
+        return talkBackInfoVO;
     }
 }
